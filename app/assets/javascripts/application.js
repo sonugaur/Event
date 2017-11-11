3454f3256bdabb22781f78641c90408d938bd6ce
Sonu Gaur <akshgaur001@gmail.com>
2017-11-11 17:56:41 +0530

    Heroku config

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 
-//= require bootstrap
+
 //= require rails-ujs
 //= require turbolinks
+//= require jquery
+//= require jquery_ujs
 //= require_tree .
+
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Signup
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    # config.assets.enabled = true
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
